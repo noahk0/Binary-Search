85103b0b1524c18a891c64101e2a4e0d4eaeec00
noahk0 <noah.kim@mail.utoronto.ca>
2024-07-17 19:45:58 +0900

    Create #medium Search in Rotated Sorted Array.py

---
+++ #medium Search in Rotated Sorted Array.py
@@ -0,0 +1,19 @@
+def search(self, nums: List[int], target: int) -> int:
+    l, r = 0, len(nums) - 1
+
+    while l <= r:
+        mid = (l + r) // 2
+
+        if target == nums[mid]:
+            return mid
+
+        if nums[l] <= target < nums[mid]:
+            r = mid -1
+        elif nums[mid] < target <= nums[r]:
+            l = mid + 1
+        elif nums[mid] < nums[l]:
+            r = mid - 1
+        else:
+            l = mid + 1
+
+    return -1
