0498a93ffa5a22e9962083e5d5488b82c2f9e1cf
noahk0 <noah.kim@mail.utoronto.ca>
2024-07-20 18:58:50 +0900

    Create hard Median of Two Sorted Arrays.py

---
+++ hard Median of Two Sorted Arrays.py
@@ -0,0 +1,21 @@
+def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+    if len(nums2) < len(nums1):
+        nums1, nums2 = nums2, nums1
+
+    start, end, leng = 0, len(nums1) - 1, (len(nums1) + len(nums2)) // 2
+
+    while True:
+        mid1 = (start + end) // 2
+        mid2 = leng - mid1 - 2
+
+        pre1 = nums1[mid1] if 0 <= mid1 else float('-inf')
+        pre2 = nums2[mid2] if 0 <= mid2 else float('-inf')
+        nxt1 = nums1[mid1 + 1] if mid1 + 1 < len(nums1) else float('inf')
+        nxt2 = nums2[mid2 + 1] if mid2 + 1 < len(nums2) else float('inf')
+
+        if pre1 <= nxt2 and pre2 <= nxt1:
+            return min(nxt1, nxt2) if (len(nums1) + len(nums2)) % 2 else (max(pre1, pre2) + min(nxt1, nxt2)) / 2
+        elif nxt2 < pre1:
+            end = mid1 - 1
+        else:
+            start = mid1 + 1
